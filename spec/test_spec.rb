51e8aab0494a19b8a3bea43e54e29303c9109642
John Kisor <jkisor@gmail.com>
2019-01-29 08:50:34 -0500

    working

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,6 +1,6 @@
 def score_roll(roll, category)
   return 1 if roll.include?(1)
-  
+
   0
 end
 
@@ -20,8 +20,19 @@ describe "scoring a role in the ones category" do
   end
 
   context "when a single one is rolled" do
-    it "scores 1" do
+    it "scores sum of ones" do
       roll = [1,2,3,4,5]
+      category = :ones
+
+      score = score_roll(roll, category)
+
+      expect(score).to eq(1)
+    end
+  end
+
+  context "when a many ones are rolled" do
+    it "scores sum of ones" do
+      roll = [1,1,1,1,1]
       category = :ones
 
       score = score_roll(roll, category)
