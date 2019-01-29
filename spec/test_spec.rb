c426e8b6bf11d51ad58a902f8477e3e967d9509c
John Kisor <jkisor@gmail.com>
2019-01-29 08:44:56 -0500

    working

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,6 +1,12 @@
+def score(roll, category)
+end
 
 describe "scoring a role in the ones category" do
   it "" do
+    roll = [2,3,4,5,6]
+    category = :ones
+    
+    score(roll, category)
     expect(true).to eq(true)
   end
 end
