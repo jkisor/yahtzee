b44b9b7a860ad893cbb8d10dd1b6d29bda763849
John Kisor <jkisor@gmail.com>
2019-01-29 08:46:54 -0500

    working

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -3,7 +3,9 @@ def score_roll(roll, category)
 end
 
 describe "scoring a role in the ones category" do
+
   context "when no ones where rolled" do
+  
     it "scores 0" do
       roll = [2,3,4,5,6]
       category = :ones
@@ -12,6 +14,7 @@ describe "scoring a role in the ones category" do
   
       expect(score).to eq(0)
     end
+  
   end  
 
 end
