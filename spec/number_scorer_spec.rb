760d496c772d72b0a57c0fe9801f6d4801c00ec5
John Kisor <jkisor@gmail.com>
2019-01-29 13:46:05 -0500

    working

---
+++ spec/number_scorer_spec.rb
@@ -0,0 +1,44 @@
+require "./number_scorer"
+
+describe NumberScorer do
+
+  context "when no occurances of number are rolled" do
+  
+    it "scores 0" do
+      roll = [2,3,4,5,6]
+      scorer = NumberScorer.new(1)
+  
+      score = scorer.score(roll)
+  
+      expect(score).to eq(0)
+    end
+  
+  end
+
+  context "when a single occurance of number is rolled" do
+
+    it "scores sum of occurances" do
+      roll = [1,2,3,4,5]
+      scorer = NumberScorer.new(1)
+
+      score = scorer.score(roll)
+
+      expect(score).to eq(1)
+    end
+
+  end
+
+  context "when many occurances of number are rolled" do
+
+    it "scores sum of occurances" do
+      roll = [1,1,1,1,1]
+      scorer = NumberScorer.new(1)
+
+      score = scorer.score(roll)
+
+      expect(score).to eq(5)
+    end
+
+  end
+
+end
