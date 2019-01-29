ac4540b28e77c5f98974e08d627a03a287ecd2e3
John Kisor <jkisor@gmail.com>
2019-01-29 14:20:46 -0500

    working

--- spec/scorer_spec.rb
+++ spec/scorer_spec.rb
@@ -3,9 +3,11 @@ require "./scorer"
 describe Scorer do
   subject { described_class }
 
-  it do
-    scorer = subject.for_category(:ones)
-    expect(scorer).to be_a NumberScorer
-  end
-
+  context "for ones category" do
+    it "creates a scorer" do
+      scorer = subject.for_category(:ones)
+      expect(scorer).to be_a NumberScorer
+    end
+  end  
+  
 end
