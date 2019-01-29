344d5c21d7f6c158e2dd69f8300b4286db493a15
John Kisor <jkisor@gmail.com>
2019-01-29 15:02:06 -0500

    working

--- two_pair_scorer.rb
+++ two_pair_scorer.rb
@@ -3,6 +3,8 @@ class TwoPairScorer
   def score(roll)
     pairs = roll.group_by { |number| number }.select { |k,v| v.count >= 2 }
     
+    return 0 if pairs.keys.count < 2
+
     pairs.keys.map { |x| x * 2 }.reduce(0, :+)
   end
 
