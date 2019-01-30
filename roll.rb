5be3077b9689a69a66074fd797c0b5d45010efe0
John Kisor <jkisor@gmail.com>
2019-01-29 20:33:52 -0500

    working

--- roll.rb
+++ roll.rb
@@ -6,17 +6,21 @@ class Roll
   end
 
   def pairs
-    grouped_numbers.select { |k,v| v.count >= 2 }.keys
+    n_of_a_kind(2)
   end
 
   def trios
-    grouped_numbers.select { |k,v| v.count >= 3 }.keys
+    n_of_a_kind(3)
   end
 
   private
 
   def grouped_numbers
     @numbers.group_by { |number| number }
+  end
+
+  def n_of_a_kind(n)
+    grouped_numbers.select { |k,v| v.count >= n }.keys
   end
 
 end
