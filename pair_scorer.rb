b4876cbc077bae800715909d609d5291d6e08858
John Kisor <jkisor@gmail.com>
2019-01-29 20:17:04 -0500

    working

--- pair_scorer.rb
+++ pair_scorer.rb
@@ -1,14 +1,11 @@
+require "./roll"
+
 class PairScorer
   
   def score(roll)
-    (pairs(roll).max || 0) * 2
+    roll = Roll.new(roll)
+
+    (roll.pairs.max || 0) * 2
   end
-
-  private
-
-  def pairs(roll)
-    grouped = roll.group_by { |number| number }
-    grouped.select { |k,v| v.count >= 2 }.keys
-  end
-
+  
 end
