ef83995dbe523d6ef2dabf2b09269a7709b50a53
John Kisor <jkisor@gmail.com>
2019-01-30 07:24:07 -0500

    working

--- full_house.rb
+++ full_house.rb
@@ -1,7 +1,14 @@
 class FullHouse
 
   def score(dice)
-    0
+    grouped_numbers = dice.group_by { |number| number }
+    
+    
+    pair = grouped_numbers.select { |k,v| v.count == 2 }
+    trio = grouped_numbers.select { |k,v| v.count == 3 }
+
+    return 0 if pair.keys.empty? || trio.keys.empty?
+    
   end
 
 end
